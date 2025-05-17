9bdc75a9519190c0d18c84f2103118139efca7b9
ujjwalgoel23 <ujjwalgoel2711@gmail.com>
2025-05-17 22:07:26 +0530

    first

--- Datebasic.js
+++ Datebasic.js
@@ -1,8 +1,24 @@
-const d=new Date();
-console.log(d);
-console.log(d.getDate());
-console.log(d.toDateString());
-console.log(d.toString());
-console.log(typeof d);
+// const d=new Date();
+// console.log(d);
+// console.log(d.getDate());//or
+// const now=Date.now();
+// console.log(now);
+// console.log(d.toDateString());
+// console.log(d.toString());
 
-//
+// const da= new Date("2024-01-12");
+// console.log(da);
+
+const d=new Date;
+d.setDate(20);
+d.setFullYear(2022);
+d.setMonth(3);
+console.log(d);
+console.log(d.toLocaleDateString());
+console.log(d.toString());  
+
+//date calculation
+const date1=new Date();
+const date2= new Date("2025-04-20");
+console.log(date2-date1);//ans milisecond me milegaa
+ 
