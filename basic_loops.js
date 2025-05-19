e79cf8d341dbe660d68e15bf87af5a5cc20f4721
ujjwalgoel23 <ujjwalgoel2711@gmail.com>
2025-05-19 19:14:50 +0530

    first

---
+++ basic_loops.js
@@ -0,0 +1,43 @@
+ // //FOR LOOP
+// for(let i=0;i<20;i++){
+//     console.log("hello");
+// }
+// //using nested loop:->
+// for(let i=0;i<6;i++){
+//     let string="";
+//   for(let j=1;j<=5;j++){
+//     string+=j+" ";
+//   }
+//   console.log(string);
+// }
+
+//WHILE LOOP-->
+// let i=0;
+// while(i<5){
+//     console.log("hello");
+//     i++;
+// }
+   
+//DO-WHILE LOOP
+// let i=0;
+// do{
+//     console.log("bye");
+//     i++;
+// }while(i<5);
+
+
+// //callback function
+// function name(fun){
+//  console.log("hello");
+//  fun();
+// }
+// function greet(){
+//     console.log("world");
+// }
+// name(greet);
+
+// function fetchdata(){
+//     console.log("data fetched");
+// }
+// setInterval(fetchdata,2000);
+
---
+++ functionBasic.js
@@ -0,0 +1,38 @@
+// //function definition 
+// function greet(){
+//     console.log("hello coder army");
+// }
+// //function calling-->
+// greet();
+
+// program to add 2  number with parameters
+// function sum(number1,number2){
+//     console.log(number1+number2) ;
+// }
+// sum(4,5);//argument
+
+//program to multiply 2 numbers
+// function multiple(number1,number2){
+//     console.log(number1*number2);
+// }
+// multiple(4,5);
+
+// //ANOTHER WAY TO CREATE FUN()-->
+// const fun1=function(){
+//     console.log("hello");
+// }
+// fun1();
+
+// //ARROW FUNCTION:->
+// const fun=()=>{
+// console.log("hello coder");
+// }
+// fun();
+
+//REST OPERATOR:->
+const sum=(...num)=>{
+    console.log(num);
+}
+
+sum(3,4);
+sum(4,6,7,8);
