60b73d4986a55696ed4e64c05ee41be73b6d9188
ujjwalgoel23 <ujjwalgoel2711@gmail.com>
2025-05-21 20:20:57 +0530

    first

--- DOM/createElement.js
+++ DOM/createElement.js
@@ -8,14 +8,34 @@
 // const parent=document.getElementById("root");
 // parent.appendChild(element);
 
-//attaching multiple element->
+// //attaching multiple element->
 // function attach(content){
 // let element=document.createElement('li');
-
 // element.innerHTML=content;
 // const parent=document.getElementById("root");
-// parent.appendChild(element);
+// // parent.appendChild(element);
+// parent.append(element,"hello world"); // is also used to attach multiple node at single time
 // }
 // attach("ts");
-// attach("react");
+// // attach("react");
 
+//******************************************** */
+// create a textnode->
+// const element=document.createTextNode("hello coders");
+// const parent=document.getElementById("root");
+// parent.append(element);
+
+//******************************************** */
+// //create a attribute node-->
+//  const element=document.createAttribute("id");
+//  element.value= "first" ;
+
+//access first element
+//  const parent=document.querySelector('li');
+//  parent.setAttributeNode(element);
+
+//access second element
+// const parent=document.getElementById("root");
+// parent.children[1].setAttributeNode(element);
+
+//**************************************************/
