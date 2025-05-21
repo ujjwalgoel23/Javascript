b166d24cc920e6fbb7ed125a2dd52c57c26e4c86
ujjwalgoel23 <ujjwalgoel2711@gmail.com>
2025-05-21 19:02:43 +0530

    first

---
+++ DOM/createElement.js
@@ -0,0 +1,21 @@
+// we learn how we can create a element using javascipt
+//we want to create <li>typescript<li> using js
+
+//created an element
+// let element=document.createElement('li');
+// element.innerHTML="typescript";
+
+// const parent=document.getElementById("root");
+// parent.appendChild(element);
+
+//attaching multiple element->
+// function attach(content){
+// let element=document.createElement('li');
+
+// element.innerHTML=content;
+// const parent=document.getElementById("root");
+// parent.appendChild(element);
+// }
+// attach("ts");
+// attach("react");
+
--- DOM/first.js
+++ DOM/first.js
@@ -1,3 +1,17 @@
 // document.getElementById("first");
 // const id=document.querySelector('#first');
 // id.innerHTML="hello";
+
+//convert nodelist into array
+// array.from(obj); 
+
+//accessing by tagname:->
+// let team=document.getElementsByTagName('li');
+// for(let i=0;i<team.length;i++)
+//    team[i].innerHTML="hello";
+
+const par=document.querySelector('ul');
+console.log(par);
+console.log(par.childNodes);//it gives the list 
+//it gives html node and text node also text node include the empty space between the node
+ 
