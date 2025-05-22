20327b75b0eecd3418350ed006f083e91484d200
ujjwalgoel23 <ujjwalgoel2711@gmail.com>
2025-05-22 15:23:36 +0530

    first

---
+++ projects/backgroungjs.js
@@ -0,0 +1,41 @@
+//FIRST METHOD---->
+// const id1=document.querySelector("#red");
+// const id2=document.querySelector("#blue");
+// const id3=document.querySelector("#yellow");
+// const id4=document.querySelector("#green");
+// const id5=document.querySelector("#orange");
+// const body=document.body;
+
+// id1.addEventListener("click",()=>{
+//     body.style.backgroundColor="red";
+// })
+
+// id2.addEventListener("click",()=>{
+//    body.style.backgroundColor="blue";
+// })
+
+// id3.addEventListener("click",()=>{
+//    body.style.backgroundColor = "yellow";
+// })
+
+// id4.addEventListener("click",()=>{
+//   body.style.backgroundColor = "green";
+// })
+
+// id5.addEventListener("click",()=>{
+//   body.style.backgroundColor = "orange";
+// })
+
+/**********************************8******* */
+//SECOND METHOD-->
+
+const button=document.querySelectorAll("button");
+const body=document.body;
+
+button.forEach((button)=>{
+    button.addEventListener("click",()=>{
+        body.style.backgroundColor=button.id;
+    })
+})
+
+
