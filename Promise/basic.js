1abd3bfee1de75560f623f6af6e7a53622c22d5d
ujjwalgoel23 <ujjwalgoel2711@gmail.com>
2025-06-01 13:28:13 +0530

    first

--- Promise/basic.js
+++ Promise/basic.js
@@ -41,6 +41,6 @@ const obj=fetch('http://api.weatherapi.com/v1/current.json?key=2321325ebd9b474bb
         return response.json();
     }).then((data)=>{
         console.log(data);
-    });
+    });//this method is  also known as promise chaining
+     
 
-    
